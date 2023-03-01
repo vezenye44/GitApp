db45e8e7eb1e83bca8a94befde1c66369d987398
Alecsey Efimov <alicsey2000@gmail.com>
2023-03-01 21:31:07 +0300

    Добавлена загрузка аватаров с помощью Coil + незначительные изменения в разметке.

--- app/src/main/java/com/example/gitapp/UserViewHolder.kt
+++ app/src/main/java/com/example/gitapp/UserViewHolder.kt
@@ -3,6 +3,8 @@ package com.example.gitapp
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import coil.load
+import coil.transform.CircleCropTransformation
 import com.example.gitapp.databinding.UsersRecyclerViewItemBinding
 
 class UserViewHolder(parent: ViewGroup) : RecyclerView.ViewHolder(
@@ -11,8 +13,12 @@ class UserViewHolder(parent: ViewGroup) : RecyclerView.ViewHolder(
 
     private val binding = UsersRecyclerViewItemBinding.bind(itemView)
 
-    fun bind(user : UserDTO) {
-        binding.usersRecyclerItemAvatarImageview
+    fun bind(user: UserDTO) {
+        binding.usersRecyclerItemAvatarImageview.load(user.avatar_url){
+            crossfade(true)
+            placeholder(R.drawable.ic_image_placeholder)
+            error(R.drawable.ic_image_error)
+        }
         binding.usersRecyclerItemLoginTextview.text = user.login
         binding.usersRecyclerItemIdTextview.text = user.id.toString()
     }
--- app/src/main/java/com/example/gitapp/UsersAdapter.kt
+++ app/src/main/java/com/example/gitapp/UsersAdapter.kt
@@ -3,13 +3,14 @@ package com.example.gitapp
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
-class UsersAdapter: RecyclerView.Adapter<UserViewHolder>() {
+class UsersAdapter : RecyclerView.Adapter<UserViewHolder>() {
 
     private lateinit var userList: List<UserDTO>
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = UserViewHolder(parent)
 
-    override fun onBindViewHolder(holder: UserViewHolder, position: Int) = holder.bind(getItemByPosition(position))
+    override fun onBindViewHolder(holder: UserViewHolder, position: Int) =
+        holder.bind(getItemByPosition(position))
 
     override fun getItemCount() = userList.size
 
